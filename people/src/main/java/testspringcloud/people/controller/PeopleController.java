dadc9f80c913802b361610e81b40649cdb105520
Martin Steffen <martin.steffen@globant.com>
2019-07-19 17:56:11 -0300

    Add sleuth to trace logs.

--- people/src/main/java/testspringcloud/people/controller/PeopleController.java
+++ people/src/main/java/testspringcloud/people/controller/PeopleController.java
@@ -1,5 +1,7 @@
 package testspringcloud.people.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,12 @@ import testspringcloud.domain.People;
 @RestController
 public class PeopleController {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(PeopleController.class);
+
+
   @GetMapping("/peoples/{id}")
   public People getAll(@PathVariable("id") Integer id) {
+    LOGGER.info("Get all people method invoked.");
     return new People(id, "sergio", 22);
   }
 }
