d74ba0df668f45232595b2962766f53b22b306f6
Martin Steffen <martin.steffen@globant.com>
2019-07-23 16:13:44 -0300

    update all the microservices to use service-discovery to get the config-server location.

---
+++ people/src/main/java/testspringcloud/people/config/AppConfig.java
@@ -0,0 +1,12 @@
+package testspringcloud.people.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties("people")
+public class AppConfig {
+  private String myValue;
+}
--- people/src/main/java/testspringcloud/people/controller/PeopleController.java
+++ people/src/main/java/testspringcloud/people/controller/PeopleController.java
@@ -6,16 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import testspringcloud.domain.People;
+import testspringcloud.people.config.AppConfig;
 
 @RestController
 public class PeopleController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(PeopleController.class);
 
+  private final AppConfig appConfig;
+
+  public PeopleController(AppConfig appConfig) {
+    this.appConfig = appConfig;
+  }
 
   @GetMapping("/peoples/{id}")
   public People getAll(@PathVariable("id") Integer id) {
     LOGGER.info("Get all people method invoked.");
-    return new People(id, "sergio", 22);
+    return new People(id, "sergio:" + appConfig.getMyValue(), 22);
   }
 }
