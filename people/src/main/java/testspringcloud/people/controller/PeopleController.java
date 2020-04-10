631d38f16f069fa94d5f108d7391a28f95bfe285
Martin Steffen <martins1930@gmail.com>
2020-04-10 17:10:32 -0300

    Add database connection to "people" service. Save Idea run configurations in git repository. Create file to test http endpoints.

--- people/src/main/java/testspringcloud/people/controller/PeopleController.java
+++ people/src/main/java/testspringcloud/people/controller/PeopleController.java
@@ -8,20 +8,33 @@ import org.springframework.web.bind.annotation.RestController;
 import testspringcloud.domain.People;
 import testspringcloud.people.config.AppConfig;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @RestController
 public class PeopleController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(PeopleController.class);
 
   private final AppConfig appConfig;
+  private final DataSource dataSource;
 
-  public PeopleController(AppConfig appConfig) {
+  public PeopleController(AppConfig appConfig, DataSource dataSource) {
     this.appConfig = appConfig;
+    this.dataSource = dataSource;
   }
 
   @GetMapping("/peoples/{id}")
-  public People getAll(@PathVariable("id") Integer id) {
+  public People getAll(@PathVariable("id") Integer id) throws SQLException {
+    Connection connection = dataSource.getConnection();
+    PreparedStatement preparedStatement = connection.prepareStatement("select count(*) as tot from actor");
+    ResultSet resultSet = preparedStatement.executeQuery();
+    resultSet.next();
+    int tot = resultSet.getInt("tot");
     LOGGER.info("Get all people method invoked.");
-    return new People(id, "sergio:" + appConfig.getMyValue(), 22);
+    return new People(id, "sergio:" + appConfig.getMyValue() + "-tot="+tot, 22);
   }
 }
